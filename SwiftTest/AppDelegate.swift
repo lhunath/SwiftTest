5b7ca841e14d1de17ae38b71d228824c5210e076
Maarten Billemont <lhunath@lyndir.com>
2018-04-08 09:15:20 -0400

    Barebone Swift project.

---
+++ SwiftTest/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SwiftTest
+//
+//  Created by Maarten Billemont on 2018-04-08.
+//  Copyright © 2018 Lyndir. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Maarten Billemont on 2018-04-08.
+//  Copyright © 2018 Lyndir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func loadView() {
+        super.loadView()
+    }
+}
